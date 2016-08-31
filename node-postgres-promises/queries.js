b31632379b1f71d5a6c3f3552a603e2c08ecd2c4
Donald Lessells <donald.lessells@gmail.com>
2016-08-31 15:20:00 +0100

    fixed bugs with database and seed data setup

--- node-postgres-promises/queries.js
+++ node-postgres-promises/queries.js
@@ -6,17 +6,17 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/puppies';
+var connectionString = 'postgres://localhost:5432/students_express_react_database';
 var db = pgp(connectionString);
 
-function getAllPuppies(req, res, next) {
-  db.any('select * from pups')
+function getAllStudents(req, res, next) {
+  db.any('select * from students')
     .then(function (data) {
       res.status(200)
         .json({
           status: 'success',
           data: data,
-          message: 'Retrieved ALL puppies'
+          message: 'Retrieved ALL students'
         });
     })
     .catch(function (err) {
@@ -24,76 +24,76 @@ function getAllPuppies(req, res, next) {
     });
 }
 
-function getSinglePuppy(req, res, next) {
-  var pupID = parseInt(req.params.id);
-  db.one('select * from pups where id = $1', pupID)
-    .then(function (data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved ONE puppy'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
-    });
-}
-
-function createPuppy(req, res, next) {
-  req.body.age = parseInt(req.body.age);
-  db.none('insert into pups(name, breed, age, sex)' +
-      'values(${name}, ${breed}, ${age}, ${sex})',
-    req.body)
-    .then(function () {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: 'Inserted one puppy'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
-    });
-}
-
-function updatePuppy(req, res, next) {
-  db.none('update pups set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
-    [req.body.name, req.body.breed, parseInt(req.body.age),
-      req.body.sex, parseInt(req.params.id)])
-    .then(function () {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: 'Updated puppy'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
-    });
-}
-
-function removePuppy(req, res, next) {
-  var pupID = parseInt(req.params.id);
-  db.result('delete from pups where id = $1', pupID)
-    .then(function (result) {
-      /* jshint ignore:start */
-      res.status(200)
-        .json({
-          status: 'success',
-          message: `Removed ${result.rowCount} puppy`
-        });
-      /* jshint ignore:end */
-    })
-    .catch(function (err) {
-      return next(err);
-    });
-}
+// function getSinglePuppy(req, res, next) {
+//   var pupID = parseInt(req.params.id);
+//   db.one('select * from pups where id = $1', pupID)
+//     .then(function (data) {
+//       res.status(200)
+//         .json({
+//           status: 'success',
+//           data: data,
+//           message: 'Retrieved ONE puppy'
+//         });
+//     })
+//     .catch(function (err) {
+//       return next(err);
+//     });
+// }
+//
+// function createPuppy(req, res, next) {
+//   req.body.age = parseInt(req.body.age);
+//   db.none('insert into pups(name, breed, age, sex)' +
+//       'values(${name}, ${breed}, ${age}, ${sex})',
+//     req.body)
+//     .then(function () {
+//       res.status(200)
+//         .json({
+//           status: 'success',
+//           message: 'Inserted one puppy'
+//         });
+//     })
+//     .catch(function (err) {
+//       return next(err);
+//     });
+// }
+//
+// function updatePuppy(req, res, next) {
+//   db.none('update pups set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
+//     [req.body.name, req.body.breed, parseInt(req.body.age),
+//       req.body.sex, parseInt(req.params.id)])
+//     .then(function () {
+//       res.status(200)
+//         .json({
+//           status: 'success',
+//           message: 'Updated puppy'
+//         });
+//     })
+//     .catch(function (err) {
+//       return next(err);
+//     });
+// }
+//
+// function removePuppy(req, res, next) {
+//   var pupID = parseInt(req.params.id);
+//   db.result('delete from pups where id = $1', pupID)
+//     .then(function (result) {
+//       /* jshint ignore:start */
+//       res.status(200)
+//         .json({
+//           status: 'success',
+//           message: `Removed ${result.rowCount} puppy`
+//         });
+//       /* jshint ignore:end */
+//     })
+//     .catch(function (err) {
+//       return next(err);
+//     });
+// }
 
 module.exports = {
-  getAllPuppies: getAllPuppies,
-  getSinglePuppy: getSinglePuppy,
-  createPuppy: createPuppy,
-  updatePuppy: updatePuppy,
-  removePuppy: removePuppy
+  getAllStudents: getAllStudents
+  // getSinglePuppy: getSinglePuppy,
+  // createPuppy: createPuppy,
+  // updatePuppy: updatePuppy,
+  // removePuppy: removePuppy
 };
--- node-postgres-promises/students_express_react_database.sql
+++ node-postgres-promises/students_express_react_database.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS studentsExpressReact;
-CREATE DATABASE studentsExpressReact;
+DROP DATABASE IF EXISTS students_express_react_database;
+CREATE DATABASE students_express_react_database;
 
-\c studentsExpressReact;
+\c students_express_react_database;
 
 CREATE TABLE students (
   ID SERIAL4 PRIMARY KEY,
@@ -10,19 +10,19 @@ CREATE TABLE students (
   cohort INT4
 );
 
-CREATE TABLE allTasks (
+CREATE TABLE all_tasks (
   id SERIAL4 PRIMARY KEY,
   descriptor VARCHAR,
   task_group_id INT4
 );
 
-CREATE TABLE completedTasks (
+CREATE TABLE completed_tasks (
   id SERIAL4 PRIMARY KEY,
-  student_id REFERENCES students(id) ON DELETE CASCADE,
-  task_id REFERENCES allTasks(id) ON DELETE CASCADE
+  student_id INT4 REFERENCES students(id) ON DELETE CASCADE,
+  task_id INT4 REFERENCES all_tasks(id) ON DELETE CASCADE
 );
 
-ALTER TABLE completedTasks ADD UNIQUE (student_id, task_id);
+ALTER TABLE completed_tasks ADD UNIQUE (student_id, task_id);
 
 INSERT INTO students (surname, first_names, cohort)
   VALUES ('Tyler', 'Mary Retriever', 3);
@@ -34,33 +34,33 @@ INSERT INTO students (surname, first_names, cohort)
   VALUES ('McFadden', 'Alison Labrador', 7);
 
 
-INSERT INTO allTasks (descriptor, task_group_id)
+INSERT INTO all_tasks (descriptor, task_group_id)
   VALUES ('A', 1);
 
-INSERT INTO allTasks (descriptor, task_group_id)
+INSERT INTO all_tasks (descriptor, task_group_id)
   VALUES ('B', 1);
 
-INSERT INTO allTasks (descriptor, task_group_id)
+INSERT INTO all_tasks (descriptor, task_group_id)
   VALUES ('C', 2);
 
-INSERT INTO allTasks (descriptor, task_group_id)
+INSERT INTO all_tasks (descriptor, task_group_id)
   VALUES ('D', 2);
 
 
-INSERT INTO completedTasks (student_id, task_id)
+INSERT INTO completed_tasks (student_id, task_id)
   VALUES (1, 1);
 
-INSERT INTO completedTasks (student_id, task_id)
+INSERT INTO completed_tasks (student_id, task_id)
   VALUES (1, 2);
 
-INSERT INTO completedTasks (student_id, task_id)
+INSERT INTO completed_tasks (student_id, task_id)
   VALUES (2, 1);
 
-INSERT INTO completedTasks (student_id, task_id)
+INSERT INTO completed_tasks (student_id, task_id)
   VALUES (2, 4);
 
-INSERT INTO completedTasks (student_id, task_id)
+INSERT INTO completed_tasks (student_id, task_id)
   VALUES (3, 2);
 
-INSERT INTO completedTasks (student_id, task_id)
+INSERT INTO completed_tasks (student_id, task_id)
   VALUES (3, 3);
